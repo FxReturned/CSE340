9c0c6d9f593f64420394a1a6c149fe34627c4ae1
FxReturned <cesar.cor.itu@gmail.com>
2025-03-01 22:11:56 -0500

    updates

--- routes/static.js
+++ routes/static.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-// Static Routes
-// Servir la carpeta "public" y sus subcarpetas de forma correcta
 router.use(express.static(path.join(__dirname, "..", "public")));
 router.use("/css", express.static(__dirname + "/../public/css"));
 router.use("/js", express.static(__dirname + "/../public/js"));
